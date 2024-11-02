f80c73af985bf8b9687aebb3ef6c8d8403cf1529
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-02 10:01:57 -0500

    Create Provider

---
+++ Pages/Provider/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Threading.Tasks;
+
+namespace SupermarketWEB.Pages.Provider
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Providers Provider { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Provider.Add(Provider);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
+
---
+++ Pages/Provider/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupermarketWEB.Pages.Provider
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
