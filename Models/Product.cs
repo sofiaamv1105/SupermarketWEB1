e91c25e4553b2057ae08667e9bf6f07d2b8813c5
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-02 01:43:44 -0500

    Modelos y configuración del DbContext

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		[Column(TypeName = "decimal (6,2)")]
+		public decimal Price { get; set; }
+		public int Stock { get; set; }
+		public int CategoryId { get; set; }
+		public ICollection<Category>? Categories { get; set; } = default!;
+	}
+}
---
+++ NewFolder/SumpermarketContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+	public class SupermarketContext : DbContext
+	{
+		public SupermarketContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
