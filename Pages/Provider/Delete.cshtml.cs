3df05837cd977f2ee610da6a3f7ba2a2716bee24
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-02 10:24:51 -0500

    Delete Providers

--- Pages/Provider/Delete.cshtml.cs
+++ Pages/Provider/Delete.cshtml.cs
@@ -1,12 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Threading.Tasks;
 
 namespace SupermarketWEB.Pages.Provider
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Providers Provider { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Provider = await _context.Provider.FirstOrDefaultAsync(m => m.ProviderID == id);
+
+            if (Provider == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Provider = await _context.Provider.FindAsync(id);
+
+            if (Provider != null)
+            {
+                _context.Provider.Remove(Provider);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("Index");
         }
     }
 }
+
