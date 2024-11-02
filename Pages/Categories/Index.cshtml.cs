dbc7bbaeef1a990d06a6acb5a21ee68a4cf18efd
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-02 01:46:38 -0500

    Index Categories

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Data.SqlClient;
+
+
+namespace SupermarketWEB.Pages.Categories
+{
+	public class IndexModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public IndexModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Category> Categories { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			if (_context.Categories != null)
+			{
+				Categories = await _context.Categories.ToListAsync();
+			}
+		}
+	}
+}
