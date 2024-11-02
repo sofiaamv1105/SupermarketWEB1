42451d65bd21639390b42952b996052de926c321
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-02 02:19:23 -0500

    Create  Categories

---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+	public class CreateModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public CreateModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+		public Category Category { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Categories == null || Category == null)
+			{
+				return Page();
+			}
+
+			_context.Categories.Add(Category);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
