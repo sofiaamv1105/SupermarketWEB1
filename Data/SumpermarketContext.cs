600dfcae59dd9f922bfdf5651a3a508aab936ebf
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-08 20:13:10 -0500

    Tabla User creada y modelo account

--- Data/SumpermarketContext.cs
+++ Data/SumpermarketContext.cs
@@ -13,6 +13,7 @@ namespace SupermarketWEB.Data
 		public DbSet<Category> Categories { get; set; }
         public DbSet<PayModeModels> PayModes { get; set; }
         public DbSet<Providers> Provider { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
+{
+    public class User
+    {
+      
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+    }
+}
