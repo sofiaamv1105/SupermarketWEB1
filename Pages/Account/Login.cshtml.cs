7dbc3c34099d53314494610171d6b932ebd73e20
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-08 20:33:57 -0500

    Se creo autenticacion para index

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,43 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Models;
+using System.Security.Claims;
 
 namespace SupermarketWEB.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public User User { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            {
+                //Se crea los Claim, datos a almacenar en la Cookie
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,User.Email),
+                };
+                //se asocian los claims creados a un nombre de un cookie
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                //se agrega la identidad creada al ClaimsPrincipal de la aplicacion
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                // se registra exitosamente la autenticación y se crea la cookie en el navegador 
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+            ModelState.AddModelError(string.Empty, "Credenciales incorrectas. Por favor, intente de nuevo.");
+
+            return Page();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,11 +12,13 @@ namespace SupermarketWEB
 			// Add services to the container.
 			builder.Services.AddRazorPages();
 
-			builder.Services.AddDbContext<SupermarketContext>(options =>
-			   options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
-			   );
+            builder.Services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+            {
+                options.Cookie.Name = "MyCookieAuth";
+                options.LoginPath = "/Account/Login";
+            });
 
-			var app = builder.Build();
+            var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
